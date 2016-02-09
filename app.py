dc296938dbaa1c886733d91842eed35178a9c755
BriungRi <brnli7@gmail.com>
2016-02-08 23:07:50 -0500

    POST functionality added for data retrieval

--- app.py
+++ app.py
@@ -8,11 +8,24 @@ def default_sentence():
     pe = punengine.PunEngine("Pun")
     sentences = pe.makeArray()
     return str(sentences[random.randint(0, len(sentences))])
+
 @app.route('/punengine/api/v1.0/<string:word>')
 def generate_sentence(word):
     pe = punengine.PunEngine(word)
     sentences = pe.makeArray()
     return str(sentences[random.randint(0, len(sentences))])
+
+@app.route('/punengine/api/v1.0/data', methods=['POST']) #test with: curl -i -H "Content-Type: application/json" -X POST -d '{"orig":"hi", "new":"hello", "sentence":"this is a sentence"}' http://0.0.0.0:5000/punengine/api/v1.0/data
+def add_data():
+    if not request.json:
+        abort(400)
+    data = {
+        'orig' : request.json['orig'],
+        'new' : request.json['new'],
+        'sentence' : request.json['sentence']
+    }
+    return str(data)
+
 
 
 if __name__ == '__main__':
