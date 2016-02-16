5400a7ddde290d73a292986f6e9c0cb7774ae85a
BriungRi <brnli7@gmail.com>
2016-02-15 20:33:38 -0500

    cleaned special symbols in get_synonyms

--- dictionary.py
+++ dictionary.py
@@ -1,5 +1,6 @@
 import requests
 import enchant
+import re
 from nltk.stem import PorterStemmer
 
 def is_word(word): #Checks if it is a word
@@ -31,7 +32,7 @@ def get_example_sentences(word): #Returns an array of example sentences
         listSentences.append(page[first + 23:last])
         first = page.find("partner-example-text", last)
         last = page.find("</p>", first)
-    return listSentences    
+    return listSentences
 
 def get_similars(word): #uses spellchecker and truncation to get similar words
     list_similars = set()
@@ -46,13 +47,18 @@ def get_similars(word): #uses spellchecker and truncation to get similar words
 
 #TODO clean special symbols
 def get_synonyms(word): #finds synonyms of any word
+    first_list_synonyms = set()
     list_synonyms = set()
     urlname = "http://www.thesaurus.com/browse/" + word
     page = requests.get(urlname).text
     first = page.find("<a href=\"http://www.thesaurus.com/browse/")
     last = page.find("\"", first + 20)
     while(first > 0):
-        list_synonyms.add(page[first + 41:last])
+        first_list_synonyms.add(page[first + 41:last])
         first = page.find("<a href=\"http://www.thesaurus.com/browse/", last)
         last = page.find("\"", first + 20)
+    for i in first_list_synonyms:
+        temp = re.sub(r"\W+", " ", i)
+        temp = re.sub(r"\d+", "", temp)
+        list_synonyms.add(temp)
     return list_synonyms
